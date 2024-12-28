3db08fc45baaabf11498d3f48e82d03741cb20ee
Tamirat Dejenie <tamirat.dejenie@gmail.com>
2024-12-28 10:15:00 +0300

    css fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,136 +1,8 @@
 import { useEffect, useState } from 'react';
+import { mock_tasks, TaskProps } from './tasksmodel';
 import './App.css';
 
-type TaskRowProps = {
-  taskName: string;
-  taskTime: string;
-  taskDuration: string;
-}
-
-const mock_tasks = [{
-    taskName: 'Task 1',
-    taskTime: '10:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 2',
-    taskTime: '12:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 3',
-    taskTime: '13:00',
-    taskDuration: '3h'
-  },{
-    taskName: 'Task 4',
-    taskTime: '16:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 5',
-    taskTime: '18:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 6',
-    taskTime: '19:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 7',
-    taskTime: '21:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 8',
-    taskTime: '22:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 9',
-    taskTime: '00:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 10',
-    taskTime: '01:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 11',
-    taskTime: '03:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 12',
-    taskTime: '04:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 13',
-    taskTime: '06:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 14',
-    taskTime: '07:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 15',
-    taskTime: '09:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 16',
-    taskTime: '10:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 17',
-    taskTime: '12:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 18',
-    taskTime: '13:00',
-    taskDuration: '3h'
-  },{
-    taskName: 'Task 19',
-    taskTime: '16:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 20',
-    taskTime: '18:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 21',
-    taskTime: '19:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 22',
-    taskTime: '21:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 23',
-    taskTime: '22:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 24',
-    taskTime: '00:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 25',
-    taskTime: '01:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 26',
-    taskTime: '03:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 27',
-    taskTime: '04:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 28',
-    taskTime: '06:00',
-    taskDuration: '1h'
-  },{
-    taskName: 'Task 29',
-    taskTime: '07:00',
-    taskDuration: '2h'
-  },{
-    taskName: 'Task 30',
-    taskTime: '09:00',
-    taskDuration: '1h'
-  }
-];
-
-const TaskRow = ({ taskName, taskTime, taskDuration }: TaskRowProps) => {
+const TaskRow = ({ taskName, taskTime, taskDuration }: TaskProps) => {
   return (
     <div className='task-row'>
       <div className="task-name">{taskName}</div>
@@ -141,31 +13,33 @@ const TaskRow = ({ taskName, taskTime, taskDuration }: TaskRowProps) => {
 };
 
 function App() {
-  const [tasks, setTasks] = useState(mock_tasks);
+  const [tasks, setTasks] = useState(mock_tasks.slice(0, 5));
   const [canvaHeight, setCanvaHeight] = useState(0);
 
   useEffect(() => {
     setTasks(tasks);
-    setCanvaHeight(tasks.length * 15.06666);
+    setCanvaHeight(tasks.length * 34); // 34px : height of each task row
   }, [tasks]);
 
   return (
     <main>
-      <div className='container' style={
-       { height: canvaHeight + 50 } 
-      }>
-        <div className="tasks-container">
+      <div className='container'>
+        <div className="tasks-container" style={{ height: canvaHeight + 34 }}>
           <div className="tasks-header">
             <div className="task-name">Task Name</div>
             <div className="task-time" title='Start time'>Time</div>
             <div className="task-duration">Duration</div>
-            <div className="task-add" title='Add task/project'><button className="add-task-btn">+</button></div>
+            <div className="task-add" title='Add task/project'>
+              <button className="add-task-btn">+</button>
+            </div>
           </div>
-          <div className="tasks-list" style={{height: canvaHeight}} >{tasks.map((task, index) => (
+          <div className="tasks-list" style={{ height: canvaHeight + 34 }}>
+            {tasks.map((task, index) => (
               <TaskRow key={index} taskName={task.taskName} taskTime={task.taskTime} taskDuration={task.taskDuration} />
-            ))}</div>
+            ))}
+          </div>
         </div>
-        <div className="gant-container">
+        <div className="gant-container" style={{ height: canvaHeight + 34 }}>
           <div className="months-header">
             <div className="month">Jan</div>
             <div className="month">Feb</div>
@@ -180,7 +54,7 @@ function App() {
             <div className="month">Nov</div>
             <div className="month">Dec</div>
           </div>
-          <canvas className="gant-canvas" height={canvaHeight/1. } style={{backgroundColor: "ThreeDFace"}} ></canvas>
+          <canvas className="gant-canvas" style={{ height: canvaHeight, borderBottom: tasks.length == 0 ? 'none' : '1px solid rgb(138, 138, 138)' }} ></canvas>
         </div>
       </div>
     </main>
